62576e1f7534fdf3093e8565319ebfcd72171363
nichoth <nichoth@nichoth.com>
2025-11-12 15:58:55 -0800

    setup

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,11 @@
 import { type FunctionComponent, render } from 'preact'
 import { html } from 'htm/preact'
-import { example } from '../src/index.js'
-
-example()
+import '@substrate-system/css-normalize'
 
 const Example:FunctionComponent<unknown> = function () {
-    return html`<div>hello</div>`
+    return html`<div>
+        <h1>This is an example</h1>
+    </div>`
 }
 
 render(html`<${Example} />`, document.getElementById('root')!)
